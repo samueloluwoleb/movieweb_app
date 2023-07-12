37d3ec3a14a22376ea85dbe5e318549354ca902d
Samuel Oluwole <samueloluwole2005@gmail.com>
2023-07-12 09:29:25 -0400

    Working application with comments but before styling

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request
 from datamanager.json_data_manager import JSONDataManager
-import json
 
 app = Flask(__name__)
 
+
 # create an object of the JSONDataManager class
-data_manager = JSONDataManager('storage/movies_database.json')
+def object_create():
+    try:
+        return JSONDataManager('storage/movies_database.json')
+    except FileNotFoundError:
+        return render_template('404.html')
+
+
+data_manager = object_create()
 
 
 @app.route('/')
@@ -25,8 +32,11 @@ def list_users():
         It renders users.html webpage which displays users' names
     :return:
     """
-    users = data_manager.get_all_users()
-    return render_template('users.html', users=users)
+    try:
+        users = data_manager.get_all_users()
+        return render_template('users.html', users=users)
+    except (AttributeError, ValueError, TypeError, FileNotFoundError):
+        return render_template('404.html')
 
 
 @app.route('/users/<user_id>')
@@ -39,86 +49,168 @@ def show_user_movies(user_id):
     :param user_id:
     :return:
     """
-    user_movies = data_manager.get_user_movies(user_id)
-    name = data_manager.get_user_name(user_id)
-    return render_template('user_movies.html', user_movies=user_movies, user_id=user_id, name=name)
+    try:
+        user_movies = data_manager.get_user_movies(user_id)
+        name = data_manager.get_user_name(user_id)
+        return render_template('user_movies.html', user_movies=user_movies, user_id=user_id, name=name)
+    except (AttributeError, ValueError, TypeError):
+        return render_template('404.html')
 
 
 @app.route('/add_user')
 def add_user():
     """
-        Gets triggered when the client sends either a get or post request to this route.
+        Gets triggered when the client sends either a get request to this route.
+        Parses response data and renders the appropriate html document based on app state and condition statements
 
     :return:
     """
-    user_name = request.args.get('username')
-    name = ""
-    if user_name is None:
-        return render_template('add_user.html', user_name=user_name, name=name)
-    else:
-        if user_name == '':
-            return render_template('add_user.html', user_name=user_name)
-        else:
-            name = data_manager.add_user(user_name)
-            user_name = name
+    try:
+        user_name = request.args.get('username')
+        name = ""
+        if user_name is None:
             return render_template('add_user.html', user_name=user_name, name=name)
+        else:
+            if user_name == '':
+                return render_template('add_user.html', user_name=user_name)
+            else:
+                name, user_id = data_manager.add_user(user_name)
+                user_name = name
+                return render_template('add_user.html', user_name=user_name, name=name, user_id=user_id)
+    except (AttributeError, ValueError, TypeError):
+        return render_template('404.html')
 
 
 @app.route('/users/<user_id>/add_movie', methods=['GET', 'POST'])
 def add_user_movie(user_id):
-    if request.method == 'POST':
-        name = request.form['movie_name']
-        year = request.form['movie_year']
-        rating = request.form['movie_rating']
-        director = request.form['movie_director']
+    """
+        Gets triggered when the client sends either a get or post request to this route.
+        Parses response data and renders the appropriate html document based on app state and some condition statements
+    :param user_id:
+    :return:
+    """
+    try:
+        # Parses POST response data and renders the appropriate html document with the appropriate response data
+        if request.method == 'POST':
+            user_movies = data_manager.get_user_movies(user_id)
+            name = request.form['movie_name']
+            year = request.form['movie_year']
+            rating = request.form['movie_rating']
+            director = request.form['movie_director']
 
-        data_manager.add_user_movie(name, director, year, rating, user_id)
-        movie_name = "movie added"
+            # check if user entered a movie name
+            if name.strip() == '':
+                movie_name = 'empty name'
+                return render_template('add_user_movie.html', user_id=user_id, movie_name=movie_name)
 
-        return render_template('add_user_movie.html', user_id=user_id, movie_name=movie_name, name=name)
-    else:
-        movie_name = request.args.get('movie_search')
-        if movie_name is None:
-            return render_template('add_user_movie.html', user_id=user_id, movie_name=movie_name)
-        if movie_name or movie_name == '':
-            name, year, rating, director = data_manager.get_movie_info_api(movie_name, user_id)
-            if name == 'no movie found':
-                movie_name = name
+            # check if user already has the movie with the name that is inputted
+            if user_movies:
+                for movie in user_movies:
+                    if name.strip() == movie.get('name').strip():
+                        movie_name = 'movie_exists'
+                        return render_template('add_user_movie.html', user_id=user_id, movie_name=movie_name)
+
+            data_manager.add_user_movie(name, director, year, rating, user_id)
+            movie_name = "movie added"
+
+            return render_template('add_user_movie.html', user_id=user_id, movie_name=movie_name, name=name)
+        # Parses GET response data and renders the appropriate html document with the appropriate response data
+        else:
+            movie_name = request.args.get('movie_search')
+            if movie_name is None:
                 return render_template('add_user_movie.html', user_id=user_id, movie_name=movie_name)
-            if name == 'no search':
-                movie_name = name
-                return render_template('add_user_movie.html', user_id=user_id, movie_name=movie_name)
-            if name == 'movie exists':
-                movie_name = name
-                return render_template('add_user_movie.html', user_id=user_id, movie_name=movie_name)
-            return render_template('add_user_movie.html', movie_name=movie_name, year=year,
-                                   rating=rating, director=director, user_id=user_id, name=name)
+            if movie_name or movie_name == '':
+                name, year, rating, director = data_manager.get_movie_info_api(movie_name, user_id)
+                if name == 'no movie found':
+                    movie_name = name
+                    return render_template('add_user_movie.html', user_id=user_id, movie_name=movie_name)
+                if name == 'no search':
+                    movie_name = name
+                    return render_template('add_user_movie.html', user_id=user_id, movie_name=movie_name)
+                if name == 'movie exists':
+                    movie_name = name
+                    return render_template('add_user_movie.html', user_id=user_id, movie_name=movie_name)
+                return render_template('add_user_movie.html', movie_name=movie_name, year=year,
+                                       rating=rating, director=director, user_id=user_id, name=name)
+    except (AttributeError, ValueError, TypeError):
+        return render_template('404.html')
 
 
 @app.route('/users/<user_id>/update_movie/<movie_id>', methods=['GET', 'POST'])
 def update_user_movie(user_id, movie_id):
-    movie_id = int(movie_id)
-    if request.method == 'POST':
-        name = request.form['movie_name']
-        year = request.form['movie_year']
-        rating = request.form['movie_rating']
-        director = request.form['movie_director']
+    """
+        Gets triggered when the client sends either a get or post request to this route.
+        Parses response data and renders the appropriate html document based on app state and some condition statements
+    :param user_id:
+    :param movie_id:
+    :return:
+    """
+    try:
+        movie_id = int(movie_id)
 
-        data_manager.update_movie(name, director, year, rating, user_id, movie_id)
+        # Parses POST response data and renders the appropriate html document with the appropriate response data
+        if request.method == 'POST':
+            user_movies = data_manager.get_user_movies(user_id)
+            name = request.form['movie_name']
+            year = request.form['movie_year']
+            rating = request.form['movie_rating']
+            director = request.form['movie_director']
 
-        return render_template('update_user_movie.html', name=name, user_id=user_id)
-    else:
-        movie = data_manager.get_user_movie(user_id, movie_id)
-        return render_template('update_user_movie.html', user_id=user_id, movie_id=movie_id, movie=movie)
+            # check if user entered a movie name
+            if name.strip() == '':
+                movie_name = 'empty name'
+                return render_template('update_user_movie.html', movie_name=movie_name, user_id=user_id)
+
+            # check if user already has the movie with the name that is inputted
+
+            for movie in user_movies:
+                if (name.strip() == movie.get('name').strip()) and (movie.get('id') != movie_id):
+                    movie_title = 'movie exists'
+                    return render_template('update_user_movie.html', movie_title=movie_title, user_id=user_id)
+
+            data_manager.update_movie(name, director, year, rating, user_id, movie_id)
+            return render_template('update_user_movie.html', name=name, user_id=user_id)
+
+        # Parses GET response data and renders the appropriate html document with the appropriate response data
+        else:
+            movie = data_manager.get_user_movie(user_id, movie_id)
+            return render_template('update_user_movie.html', user_id=user_id, movie_id=movie_id, movie=movie)
+    except (AttributeError, ValueError, TypeError):
+        return render_template('404.html')
 
 
 @app.route('/users/<user_id>/delete_movie/<movie_id>')
 def delete_user_movie(user_id, movie_id):
-    movie_id = int(movie_id)
-    movie_name = data_manager.get_movie_name(user_id, movie_id)
-    data_manager.delete_movie(user_id, movie_id)
+    """
+        Gets triggered when the client sends a get request to the route
+        Parses response data and renders the appropriate html document based on app state and some condition statements
+    :param user_id:
+    :param movie_id:
+    :return:
+    """
+    try:
+        movie_id = int(movie_id)
+        movie_name = data_manager.get_movie_name(user_id, movie_id)
+        data_manager.delete_movie(user_id, movie_id)
 
-    return render_template('delete_user_movie.html', user_id=user_id, movie_id=movie_id, movie_name=movie_name)
+        return render_template('delete_user_movie.html', user_id=user_id, movie_id=movie_id, movie_name=movie_name)
+    except (AttributeError, ValueError, TypeError):
+        return render_template('404.html')
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(400)
+def page_not_found(e):
+    return render_template('400.html'), 400
+
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template('500.html'), 500
 
 
 if __name__ == '__main__':
--- datamanager/json_data_manager.py
+++ datamanager/json_data_manager.py
@@ -12,8 +12,11 @@ class JSONDataManager(DataManagerInterface):
         """
             Locate a JSON database file and loads it into the program
         """
-        with open(file_path, "r") as handle:
-            return json.load(handle)
+        try:
+            with open(file_path, "r") as handle:
+                return json.load(handle)
+        except (IOError, FileNotFoundError) as e:
+            print("An Error Occurred:", str(e))
 
     def __init__(self, file_path):
         """
@@ -27,8 +30,11 @@ class JSONDataManager(DataManagerInterface):
             Return a list of all users in the database
         :return:
         """
-        users = self.load_data(self._file_path)
-        return users
+        try:
+            users = self.load_data(self._file_path)
+            return users
+        except (IOError, FileNotFoundError) as e:
+            print("An Error Occurred:", str(e))
 
     def get_user_movies(self, user_id_param):
         """
@@ -37,25 +43,43 @@ class JSONDataManager(DataManagerInterface):
         :param user_id_param:
         :return:
         """
-        users = self.get_all_users()
-        return users[0].get(user_id_param).get('movies')
+        try:
+            users = self.get_all_users()
+            return users[0].get(user_id_param).get('movies')
+        except (AttributeError, ValueError, TypeError, ConnectionError) as e:
+            print("An Error Occurred:", str(e))
 
     def add_user(self, name):
-        users = self.get_all_users()
+        """
+            creates new user by taking a username parameter and auto-generating user_id, then add user to database
+        :param name:
+        :return name:
+        """
+        try:
+            users = self.get_all_users()
 
-        # creates new user_id, create a new user using their name and user_id and add it to the user database
-        new_id = max(int(user_id) for user_id in users[0]) + 1
-        new_id = str(new_id)
-        users[0][new_id] = {'name': name}
+            # generates user id, add the user name and user id to the database
+            new_id = max(int(user_id) for user_id in users[0]) + 1
+            new_id = str(new_id)
+            users[0][new_id] = {'name': name}
 
-        json_data = json.dumps(users)
+            json_data = json.dumps(users)
 
-        with open("storage/movies_database.json", "w") as fileobj:
-            fileobj.write(json_data)
+            with open("storage/movies_database.json", "w") as fileobj:
+                fileobj.write(json_data)
 
-        return name
+            return name, new_id
+        except (AttributeError, ValueError, TypeError, FileNotFoundError, ConnectionError) as e:
+            print("An Error Occurred:", str(e))
 
     def get_movie_info_api(self, movie_name_param, user_id_param):
+        """
+            Searches the web through an api for more details about a searched movie and manipulate the response
+            accordingly
+        :param movie_name_param:
+        :param user_id_param:
+        :return:
+        """
         try:
             search_title = movie_name_param
             url = f"https://www.omdbapi.com/?apikey={API_KEY_MOVIES}&t={search_title}"
@@ -77,28 +101,40 @@ class JSONDataManager(DataManagerInterface):
 
                 # check if user already has the movie that was searched
                 user_movies = self.get_user_movies(user_id_param)
-
                 if user_movies:
                     for movie in user_movies:
                         if name.strip() == movie.get('name').strip():
                             return 'movie exists', '', '', ''
 
+                # return the appropriate movie info
                 return name, year, rating, director
-        except Exception:
+        except (AttributeError, ValueError, TypeError, ConnectionError) as e:
+            print("An Error Occurred:", str(e))
             return None, None, None, None
 
     def add_user_movie(self, name_param, director_param, year_param, rating_param, user_id_param):
+        """
+            Gets some movie info parameters, parse the movie details to create a movie dictionary object and
+            add the movie dictionary object to the right user in the database
+        :param name_param:
+        :param director_param:
+        :param year_param:
+        :param rating_param:
+        :param user_id_param:
+        :return:
+        """
         try:
             users = self.get_all_users()
             movies = self.get_user_movies(user_id_param)
 
-            # create a movie id based on if user has some movies or not
+            # create a movie id based on if user already has some movies added or not
             if not movies:
                 movie_id = 1
                 movies = []
             else:
                 movie_id = int(max(movie.get('id') for movie in movies)) + 1
 
+            # create a movie dictionary object to be added to the user movie and update database file with the object
             movie_info = {
                 "id": movie_id,
                 "name": name_param,
@@ -113,60 +149,98 @@ class JSONDataManager(DataManagerInterface):
 
             with open("storage/movies_database.json", "w") as fileobj:
                 fileobj.write(json_data)
-        except Exception:
+        except (AttributeError, ValueError, TypeError, IOError, FileNotFoundError) as e:
+            print("An Error Occurred:", str(e))
             return None
 
     def get_user_movie(self, user_id_param, movie_id_param):
-        user_movies = self.get_user_movies(user_id_param)
-        for movie in user_movies:
-            if movie.get('id') == movie_id_param:
-                return movie
+        """
+            Returns the appropriate movie that belong to a user with the given user id and movie id
+        :param user_id_param:
+        :param movie_id_param:
+        :return movie:
+        """
+        try:
+            user_movies = self.get_user_movies(user_id_param)
+            for movie in user_movies:
+                if movie.get('id') == movie_id_param:
+                    return movie
+        except (AttributeError, ValueError, TypeError, ConnectionError) as e:
+            print("An Error Occurred:", str(e))
 
     def get_movie_name(self, user_id_param, movie_id_param):
-        user_movies = self.get_user_movies(user_id_param)
-        for movie in user_movies:
-            if movie.get('id') == movie_id_param:
-                return movie.get('name')
+        """
+            Returns the appropriate movie name that belong to a user with the given user id and movie id
+        :param user_id_param:
+        :param movie_id_param:
+        :return:
+        """
+        try:
+            user_movies = self.get_user_movies(user_id_param)
+            for movie in user_movies:
+                if movie.get('id') == movie_id_param:
+                    return movie.get('name')
+        except (AttributeError, ValueError, TypeError, ConnectionError) as e:
+            print("An Error Occurred:", str(e))
 
     def update_movie(self, name_param, director_param, year_param, rating_param, user_id_param, movie_id_param):
+        """
+            Gets some movie info as parameters and update the appropriate user and user movies with the new movies info
+            given the user id and movie id provided
+        :param name_param:
+        :param director_param:
+        :param year_param:
+        :param rating_param:
+        :param user_id_param:
+        :param movie_id_param:
+        :return:
+        """
+        try:
+            users = self.get_all_users()
+            movies = self.get_user_movies(user_id_param)
 
-        users = self.get_all_users()
-        movies = self.get_user_movies(user_id_param)
+            movie_info = {"id": movie_id_param, "name": name_param, "director": director_param,
+                          "year": year_param, "rating": rating_param}
+            for count, movie in enumerate(movies):
+                if movie.get('id') == movie_id_param:
+                    users[0].get(user_id_param).get("movies")[count] = movie_info
 
-        movie_info = {"id": movie_id_param, "name": name_param, "director": director_param, "year": year_param, "rating": rating_param}
-        for count, movie in enumerate(movies):
-            if movie.get('id') == movie_id_param:
-                users[0].get(user_id_param).get("movies")[count] = movie_info
-
-        json_data = json.dumps(users)
-        with open("storage/movies_database.json", "w") as fileobj:
-            fileobj.write(json_data)
+            json_data = json.dumps(users)
+            with open("storage/movies_database.json", "w") as fileobj:
+                fileobj.write(json_data)
+        except (AttributeError, ValueError, TypeError, FileNotFoundError, ConnectionError) as e:
+            print("An Error Occurred:", str(e))
 
     def delete_movie(self, user_id_param, movie_id_param):
+        """
+            Gets a user id and movie id as parameter and deletes the user movie that matches the appropriate user and
+            movie id.
+        :param user_id_param:
+        :param movie_id_param:
+        :return:
+        """
+        try:
+            users = self.get_all_users()
+            movies = self.get_user_movies(user_id_param)
 
-        users = self.get_all_users()
-        movies = self.get_user_movies(user_id_param)
+            for count, movie in enumerate(movies):
+                if movie_id_param == movie.get('id'):
+                    del users[0].get(user_id_param).get('movies')[count]
 
-        for count, movie in enumerate(movies):
-            if movie_id_param == movie.get('id'):
-                del users[0].get(user_id_param).get('movies')[count]
-
-                json_data = json.dumps(users)
-                with open("storage/movies_database.json", "w") as fileobj:
-                    fileobj.write(json_data)
+                    json_data = json.dumps(users)
+                    with open("storage/movies_database.json", "w") as fileobj:
+                        fileobj.write(json_data)
+        except (AttributeError, ValueError, TypeError, FileNotFoundError, ConnectionError) as e:
+            print("An Error Occurred:", str(e))
 
     def get_user_name(self, user_id_param):
-        users = self.get_all_users()
-        return users[0].get(user_id_param).get('name')
-
-# def find_user_by_id(self, user_id_param):
-#     users = self.get_all_users()
-#     return users[0].get(user_id_param)
-# try:
-#     movie1 = JSONDataManager('storage/movies_database.json')
-# except (TypeError, json.decoder.JSONDecodeError, FileNotFoundError):
-#     print("Check the file path, make sure it is the right json database file")
-# print(movie1.get_all_users())
-# print(movie1.get_user_movies('1'))
-# print(movie1.get_user_movies('2'))
-# print(movie1.find_user_by_id(movie1.get_all_users(), '1'))
+        """
+            Returns the appropriate user name that belong to a user with the given user id parameter
+        :param user_id_param:
+        :return:
+        """
+        try:
+            users = self.get_all_users()
+            return users[0].get(user_id_param).get('name')
+        except (AttributeError, ValueError, TypeError, FileNotFoundError, ConnectionError) as e:
+            print("An Error Occurred:", str(e))
